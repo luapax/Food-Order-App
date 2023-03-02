07ef62c50d677c86c8af0cb9e204270510c3cd32
Agata <agata.react@gmail.com>
2023-03-02 18:10:25 +0100

    Modal component and Cart v1 added

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -7,7 +7,7 @@ const Header = (props) => {
         <>
             <header className={classes.header}>
                 <h1>Food Order App</h1>
-                <HeaderCartButton />
+                <HeaderCartButton onClick={props.onShowCart} />
             </header>
             <div className={classes['main-image']}>
                 <img src={pizza} alt="Pizza" />
