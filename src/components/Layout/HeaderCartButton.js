6a60f0ed30ff477407ca6de0131ba3832800cdec
Agata <agata.react@gmail.com>
2023-03-05 22:09:39 +0100

    useContext and useReducer applied

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,23 @@
+import { useContext } from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css"
-
+import CartContext from "../../store/cart-contex";
 
 const HeaderCartButton = (props) => {
+
+    const cartCtx = useContext(CartContext)
+
+    const numberOfCartItems = cartCtx.items.reduce((current, item) => {
+        return current + item.amount;
+    }, 0);
+
     return (
         <button className={classes.button} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
             <span>Your Cart</span>
-            <span className={classes.badge}>3</span>
+            <span className={classes.badge}>{numberOfCartItems}</span>
         </button>
     )
 };
